79a2206aeb216ee7e3f3637e15b8cba4f33a889f
Diogo Braga <diogoabraga@gmail.com>
2026-06-11 01:45:24 -0300

    Force redeploy - increment version

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const GOOGLE_ADS_CONVERSION_ID = 'AW-16469027355';
 const GOOGLE_ADS_CLICK_CONVERSION = 'Hr4-CPr-rvMZEJvUhaO9';
 const GOOGLE_ADS_FORM_CONVERSION = 'KgyZCNmapLwcEJvUha09';
+const SERVER_VERSION = '2.0.0'; // Force redeployment
 
 // Logger
 const log = (message, data = '') => {
